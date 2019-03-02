07f3f0d3222a1783c570f9fc36c4b8c86f0bd4ca
Michael Eden <michael.eden@hootsuite.com>
2019-03-02 12:02:10 -0800

    Fix syntax error

---
+++ app/middleware.py
@@ -0,0 +1,14 @@
+import time
+
+def timer(handler):
+    def timer_wrapper(*args, **kwargs):
+        start = int(round(time.time() * 1000))
+        res = handler()
+        end = int(round(time.time() * 1000))
+        delta = end - start
+        print "Response time: %d ms" % delta
+        if delta > 250:
+            print "ERROR: maximum response time exceeded!"
+        elif delta > 200:
+            print "WARNING: approaching maximum response time!"
+    return timer_wrapper
