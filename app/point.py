a20eeeb8220c6b22ab3b1f0381417dfb7f04d1b1
Michael Eden <michael.eden@hootsuite.com>
2019-03-24 22:22:08 -0700

    Refactor point class

--- app/point.py
+++ app/point.py
@@ -2,17 +2,24 @@
 class Point:
     """Represents two dimensional coordinates."""
 
-    def __init__(self, data):
-        self._x = data['x']
-        self._y = data['y']
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # TODO: correct decorator.
+    @staticmethod
+    def from_json(data):
+        x = data['x']
+        y = data['y']
+
+        return Point(x, y)
 
     @staticmethod
     def get_manhattan_distance(point1, point2):
-        return abs(point1._x - point2._x) + abs(point1._y - point2._y)
+        return abs(point1.x - point2.x) + abs(point1.y - point2.y)
+
+    def get_coords(self):
+        return (self.x, self.y)
 
     def copy(self, dx = 0, dy = 0):
-        data = {
-            'x': self._x + dx,
-            'y': self._y + dy
-        }
-        return Point(data)
+        return Point(self.x + dx, self.y + dy)
